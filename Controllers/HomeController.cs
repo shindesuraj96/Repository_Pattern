427d53817b609fd2668ae62efb0429bcbba3e00c
Suraj Shinde <115707828+shindesuraj96@users.noreply.github.com>
2023-01-18 17:55:03 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Repository_Design_Pattern.Models;
+using Repository_Design_Pattern.Models.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Repository_Design_Pattern.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IProductRepository _productRepository;
+        public HomeController()
+        {
+            this._productRepository = new ProductRepository(new DataContext());
+        }
+        public ActionResult Index()
+        {
+            return View(_productRepository.GetProducts());
+        }
+        public ActionResult Create()
+        {
+            return View(new Product_Table());
+        }
+        [HttpPost]
+        public ActionResult Create(Product_Table product)
+        {
+            _productRepository.InsertProduct(product);
+            _productRepository.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Update(int Id)
+        {
+            return View(_productRepository.GetProductById(Id));
+        }
+        [HttpPost]
+        public ActionResult Update(Product_Table product)
+        {
+            _productRepository.UpdateProduct(product);
+            _productRepository.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(int Id)
+        {
+            _productRepository.DeleteProduct(Id);
+            _productRepository.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Repository_Design_Pattern.MvcApplication" Language="C#" %>
